0d65f112dbf9bcc4b139a1c7a82364ec5d4ae0ed
Elijah Ezaga <elijah.ezaga@gmail.com>
2018-11-13 21:52:39 +0100

    Form for adding matches

---
+++ src/components/Match.vue
@@ -0,0 +1,78 @@
+<template>
+    <div>
+        <h2>Save Match</h2>
+        <div class="ui form">
+
+            <!-- Home team info -->
+            <div class="fields">
+                <div class="field">
+                    <label>Home</label>
+                    <select v-model="match.homeTeam">
+                        <option v-for="team in teams" v-bind:key="team.name" v-bind:value="team">
+                            {{ team.name }}
+                        </option>
+                    </select>
+                </div>
+
+                <div class="field">
+                    <label>Goals Scored</label>
+                     <input type="text" placeholder="# goals" v-model="match.homeTeamGoals">
+                </div>
+            </div>           
+
+            <!-- Away team info -->
+            <div class="fields">
+                <div class="field">
+                    <label>Away</label>
+                    <select v-model="match.guestTeam">
+                        <option v-for="team in teams" v-bind:key="team.name" v-bind:value="team">
+                            {{ team.name }}
+                        </option>
+                    </select>
+                </div>
+
+                <div class="field">
+                    <label>Goals Scored</label>
+                     <input type="text" placeholder="# goals"  v-model="match.guestTeamGoals">
+                </div>
+            </div>
+
+            <div class="field">
+                <button class="ui button" v-on:click="saveMatch">Save</button>
+            </div>
+        </div>
+    </div>
+</template>
+    
+<script>
+import LeagueDao from '../daos/LeagueDao.js'
+import Match from '../models/Match.js'
+import Team from '../models/Team.js'
+
+const leagueDao = new LeagueDao();
+const DEFAULT_TEAM = new Team('team');
+
+export default {
+    data: function() {
+        return {
+            match: new Match(DEFAULT_TEAM, DEFAULT_TEAM, 0, 0),
+            teams: leagueDao.getTeams()
+        }
+    },
+
+    methods: {
+        saveMatch: function() {
+            leagueDao.saveMatch(-1, this.match);
+            let m = leagueDao.getMatches();
+            console.log(m[0].homeTeam.name);
+            console.log(m[0].homeTeamGoals);
+        }
+    }
+}
+</script>
+
+<style scoped>
+div.ui.form {
+    width: 700px;
+}
+</style>
