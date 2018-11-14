e43f25143800fd74ebfa4fa861cdda84d11e5979
Elijah Ezaga <elijah.ezaga@gmail.com>
2018-11-14 08:16:34 +0100

    Feature: Teams listing

--- src/components/Teams.vue
+++ src/components/Teams.vue
@@ -1,13 +1,28 @@
 <template>
-    <div>Teams.vue</div>
+    <div>
+        <h2>Soccer Teams</h2>
+        <ol>
+            <li v-for="team in teams" v-bind:key="team.name">
+                {{ team.name }}
+            </li>
+        </ol>
+    </div>
 </template>
 
 <script>
+import LeagueDao from '../daos/LeagueDao.js'
+
 export default {
-    
+    data: function() {
+        return {
+            teams: LeagueDao.getTeams()
+        }
+    }
 }
 </script>
 
 <style scoped>
-
+ol {
+    text-align: left;
+}
 </style>
